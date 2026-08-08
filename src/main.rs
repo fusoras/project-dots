b8c523ea7f8a11dc0e39a6eb2ba82759124474cf
FuSoraS <developer.fusoras@gmail.com>
2026-08-07 22:05:20 -0400

    update: bump version to v0.1.0-beta.3 for self-update testing

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use platform::Platform;
 use state::State;
 use update::check_and_perform_update;
 
-const VERSION: &str = "0.1.0-beta.2";
+const VERSION: &str = "0.1.0-beta.3";
 
 #[derive(Parser)]
 #[command(
