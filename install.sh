aec1f5ff043df4fa86d7b0e90390e5bfb269d09c
FuSoraS <developer.fusoras@gmail.com>
2026-08-18 21:02:41 -0400

    update: bump version to v0.1.0-beta.41
    Bump version to v0.1.0-beta.41 with embedded nvim-onedarkpro configuration bundle and custom binary extraction fixes.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# project-dots (v0.1.0-beta.40) System Installer Script
+# project-dots (v0.1.0-beta.41) System Installer Script
 # Usage: curl -fsSL https://raw.githubusercontent.com/fusoras/project-dots/develop/install.sh | sh
 
 # ── Dependency check ──────────────────────────────────────────
